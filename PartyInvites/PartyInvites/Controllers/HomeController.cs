af00da6c4b5b1eee31d18363487a49a79a3a5ce0
valdo174 <valdo17496@gmail.com>
2018-09-11 18:04:35 +0500

    init PartyInvites project; add guestinfo model and index view

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greetings = hour > 12 ? "Good Afternoon" : "Good Morning";
+
+            return View();
+        }
+
+    }
+}
