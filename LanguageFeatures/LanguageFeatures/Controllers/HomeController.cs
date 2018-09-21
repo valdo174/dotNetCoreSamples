bcfe678b3a27ca99af59ae895e36eb3e535f1302
Valera Nashville <valdo17496@gmail.com>
2018-09-21 21:06:29 +0500

    init LanguageFeatures

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			List<string> results = new List<string>();
+
+			foreach(Product p in Product.GetProducts())
+			{
+				string name = p?.Name;
+				decimal? price = p?.Price;
+
+				results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+			}
+
+			return View(results);
+		}
+	}
+}
